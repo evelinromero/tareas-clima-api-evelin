2047b8b0d378f0f43d6fb78f0605d601e665facf
evelinromero <romeroeve0905@gmail.com>
2026-07-08 23:17:58 -0600

    Agregar ruta DELETE para tareas

--- server.js
+++ server.js
@@ -59,4 +59,8 @@ app.get('/api/salud', (req, res) => {
   res.json({ status: 'ok' });
 });
 
+const tareasRouter = require('./routes/tareas');
+
+app.use('/api/tareas', tareasRouter);
+
 module.exports = app;
